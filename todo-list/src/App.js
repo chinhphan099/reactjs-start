39674a19a7dfc60c1a0f867b4bdac69a79e3c91d
Chinh Phan <chinhphan099@gmail.com>
2020-05-05 23:36:55 +0700

    todo app

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem/TodoItem';
 import MainMenu from './components/MainMenu/MainMenu';
@@ -13,24 +13,24 @@ class App extends Component {
         super();
 
         this.mainMenu = [
-            {iconCls: 'icon-1', title: 'Home', isActive: true},
-            {iconCls: 'icon-2', title: 'DEALS'},
-            {iconCls: 'icon-3', title: 'UPLOAD'}
+            { iconCls: 'icon-1', title: 'Home', isActive: true },
+            { iconCls: 'icon-2', title: 'DEALS' },
+            { iconCls: 'icon-3', title: 'UPLOAD' }
         ];
 
         this.table = {
             heading: ['Company', 'Contact', 'Country'],
             body: [
-                {company: 'Company 1', contact: 'Contact 1', country: 'Country 1'},
-                {company: 'Company 2', contact: 'Contact 2', country: 'Country 2'}
+                { company: 'Company 1', contact: 'Contact 1', country: 'Country 1' },
+                { company: 'Company 2', contact: 'Contact 2', country: 'Country 2' }
             ]
         };
 
         this.state = {
             currentColor: RED,
             todoItems: [
-                {title: 'Học bài', isComplete: true},
-                {title: 'Đi ngủ'}
+                { title: 'Học bài', isComplete: true },
+                { title: 'Đi ngủ' }
             ]
         };
 
@@ -42,7 +42,7 @@ class App extends Component {
     }
 
     getNextColor(color) {
-        switch(color) {
+        switch (color) {
             case RED:
                 return ORANGE;
             case ORANGE:
@@ -52,33 +52,48 @@ class App extends Component {
         }
     }
 
-    onItemClick(index) {
-        this.state.todoItems[index].isComplete = !this.state.todoItems[index].isComplete;
+    onItemClick(item) {
+        // this.state.todoItems[index].isComplete = !this.state.todoItems[index].isComplete;
+        return () => {
+            const isComplete = item.isComplete;
+            const {todoItems} = this.state;
+            const index = this.state.todoItems.indexOf(item);
+            this.setState({
+                todoItems: [
+                    ...todoItems.slice(0, index),
+                    {
+                        ...item,
+                        isComplete: !isComplete
+                    },
+                    ...todoItems.slice(index + 1)
+                ]
+            });
+        };
     }
 
     render() {
-        const {currentColor} = this.state;
+        const { currentColor } = this.state;
         return (
-            <div className="App">
-                <TrafficLight currentColor={currentColor} />
+            <div className = "App">
+                <TrafficLight currentColor={currentColor}/>
                 {
-                    this.mainMenu.map((item, index) => 
-                        <MainMenu key={index} item={item} />
+                    this.mainMenu.map((item, index) =>
+                        <MainMenu key = { index } item = { item } />
                     )
                 }
                 {
-                    this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) => 
-                        <TodoItem key={index} item={item} onClick={() => this.onItemClick(index)} />
+                    this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) =>
+                        <TodoItem key={index} item={item} onClick={this.onItemClick(item)} />
                     )
                 }
-                {this.state.todoItems.length === 0 && 'Nothing here'}
+                { this.state.todoItems.length === 0 && 'Nothing here' }
 
                 <table>
                     <thead>
                         <tr>
                             {
-                                this.table.heading.map((item, index) => 
-                                    <th key={index}>{item}</th>
+                                this.table.heading.map((item, index) =>
+                                    <th key={index}> { item } </th>
                                 )
                             }
                         </tr>
@@ -86,7 +101,10 @@ class App extends Component {
                     <tbody>
                         {
                             this.table.body.map((item, index) =>
-                                <TableRow item={item} key={index} />
+                                <
+                                TableRow item = { item }
+                                key = { index }
+                                />
                             )
                         }
                     </tbody>
